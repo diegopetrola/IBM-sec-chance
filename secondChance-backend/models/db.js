897ad87642b90535f588dab6f0094f8f2758580d
diegopetrola <diegopetrola@gmail.com>
2025-11-09 22:27:16 -0300

    Added items route and basic CRUD

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -1,6 +1,6 @@
 // db.js
-require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
@@ -9,20 +9,32 @@ let dbInstance = null;
 const dbName = `${process.env.MONGO_DB}`;
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+  if (dbInstance) {
+    return dbInstance;
+  }
 
-    const client = new MongoClient(url);      
+  try {
+    const client = new MongoClient(url, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
+    console.log("Connected to mongoDB!");
+    await client.connect();
+    dbInstance = client.db(dbName);
+    return dbInstance;
+  } catch (error) {
+    console.error("Failed to connect to mongoDB", error);
+    process.exit(1);
+  }
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+  // Task 2: Connect to database giftDB and store in variable dbInstance
+  //{{insert code}}
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-
-    // Task 3: Return database instance
-    // {{insert code}}
+  // Task 3: Return database instance
+  // {{insert code}}
 }
 
 module.exports = connectToDatabase;
